1a4bff598c8df9b5e9833b46a3054e47a63a3d16
Alexander Udalov <Alexander.Udalov@jetbrains.com>
2014-03-24 21:15:40 +0400

    Simplify codegen for trait methods with implementations
    - use existing framework (FunctionCodegen.generateMethod) which will take care
      of all subtleties of method generation (bridges, annotations, thrown
      exceptions, etc)
    - call to "generateThisOrOuter" was not needed, since when we're generating
      delegation to TImpl, "this" is always "ALOAD 0"

--- compiler/backend/src/org/jetbrains/jet/codegen/ExpressionCodegen.java
+++ compiler/backend/src/org/jetbrains/jet/codegen/ExpressionCodegen.java
@@ -2236,6 +2236,7 @@ public class ExpressionCodegen extends JetVisitor<StackValue, StackValue> implem
         throw new UnsupportedOperationException();
     }
 
+    @NotNull
     public StackValue generateThisOrOuter(@NotNull ClassDescriptor calleeContainingClass, boolean isSuper) {
         boolean isSingleton = calleeContainingClass.getKind().isSingleton();
         if (isSingleton) {
@@ -2253,8 +2254,12 @@ public class ExpressionCodegen extends JetVisitor<StackValue, StackValue> implem
         boolean inStartConstructorContext = cur instanceof ConstructorContext;
         while (cur != null) {
             ClassDescriptor thisDescriptor = cur.getThisDescriptor();
-            if (!isSuper && thisDescriptor.equals(calleeContainingClass)
-                || isSuper && DescriptorUtils.isSubclass(thisDescriptor, calleeContainingClass)) {
+
+            if (!isSuper && thisDescriptor == calleeContainingClass) {
+                return result;
+            }
+
+            if (isSuper && DescriptorUtils.isSubclass(thisDescriptor, calleeContainingClass)) {
                 return castToRequiredTypeOfInterfaceIfNeeded(result, thisDescriptor, calleeContainingClass);
             }
 
